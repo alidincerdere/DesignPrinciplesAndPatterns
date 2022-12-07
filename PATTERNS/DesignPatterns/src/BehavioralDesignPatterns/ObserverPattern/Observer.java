3ed0a8caf450cf97d85e9b3f144195fb22a51a58
SelmaDere <selmadere01@gmail.com>
2022-12-08 01:35:51 +0300

    selma reverse

--- PATTERNS/DesignPatterns/src/BehavioralDesignPatterns/ObserverPattern/Observer.java
+++ PATTERNS/DesignPatterns/src/BehavioralDesignPatterns/ObserverPattern/Observer.java
@@ -3,7 +3,6 @@ package BehavioralDesignPatterns.ObserverPattern;
 /**
  * Created by adere on 21.06.2019.
  */
-// deneme
 public interface Observer {
 
     void update(int pressure, int temperature, int humidity);
