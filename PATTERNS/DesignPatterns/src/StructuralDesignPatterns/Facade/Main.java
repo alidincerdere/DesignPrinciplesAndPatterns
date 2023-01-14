4827f0c5dcef2fe91394596b8eadb9623ec656d9
alidincerdere <ali.dincer.dere@gmail.com>
2023-01-15 00:02:53 +0300

    deneme

--- PATTERNS/DesignPatterns/src/StructuralDesignPatterns/Facade/Main.java
+++ PATTERNS/DesignPatterns/src/StructuralDesignPatterns/Facade/Main.java
@@ -4,7 +4,7 @@ package StructuralDesignPatterns.Facade;
  * Created by adere on 2.07.2019.
  */
 public class Main {
-
+// Deneme
     public static void main(String[] args) {
 
         SorterManager sorterManager = new SorterManager();
